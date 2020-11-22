01c76af7e2d03d819e9c0aa545b9e527f7963da4
tarrows <c10t@fa.ke>
2020-11-23 00:09:37 +0900

    fix script

--- scripts/fetch-stories.ts
+++ scripts/fetch-stories.ts
@@ -5,17 +5,24 @@ const ENDPOINT = "https://hacker-news.firebaseio.com/v0"
 const TOP_STORIES = `${ENDPOINT}/topstories.json`
 
 const main = async () => {
-  const topstories = await got(TOP_STORIES).json() as number[]
-  const items = await Promise.all(
-    topstories
-      .slice(0, 20)
-      .map(id => `${ENDPOINT}/items/${id}`)
-      .map(url => got(url))
-      .map(res => res.json())
-  )
+  try {
+    const topstories = await got(TOP_STORIES).json() as number[]
+    console.log(topstories.slice(0, 20))
+    const items = await Promise.all(
+      topstories
+        .slice(0, 20)
+        .map(id => `${ENDPOINT}/item/${id}.json`)
+        .map(url => got(url))
+        .map(res => res.json())
+    )
 
-  // TODO: add type
-  items.map((story: any) => fs.writeFile(`./data/${story.id}.json`, JSON.stringify(story)))
+    // TODO: add type
+    items.map((story: any) => fs.writeFile(
+      `./data/${story.id}.json`, JSON.stringify(story))
+    )
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-main().catch(err => { throw Error(err) })
+main()
