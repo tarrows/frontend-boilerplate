e9775c0c2ba1bd18f146826caa08cdf0c91314c4
tarrows <c10t@fa.ke>
2020-11-26 04:23:44 +0900

    display stories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import type { NextPage, GetStaticProps } from 'next'
 
-const IndexPage = () => <h1>It Works!</h1>
+import { PrismaClient, Story } from "@prisma/client"
+import { Stories } from '../components/Stories'
+
+type Props = {
+  stories: Story[]
+}
+
+const IndexPage: NextPage<Props> = (props) => (
+  <Stories stories={props.stories} />
+)
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prisma = new PrismaClient()
+  const stories = await prisma.story.findMany({ take: 10 })
+  const props = { stories }
+
+  return { props }
+}
 
 export default IndexPage
